bf48c2f867be798da1a6ce3ec0a8779951d01e8e
Noah Teubner <nteubner@schueler.hakzell.at>
2024-04-11 08:39:22 +0200

    Reapply "created the path for the data.sqlite"
    This reverts commit a714cc3d7d7dbd6eb1718d6828a77aeea375e13a.

---
+++ Studiensammlung.StudiensammlungCore/Services/IAlertService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studiensammlung.StudiensammlungCore.Services;
+
+public interface IAlertService
+{
+    void ShowAlert(string title, string message);
+    Task ShowAlertAsync(string title, string message);
+}
