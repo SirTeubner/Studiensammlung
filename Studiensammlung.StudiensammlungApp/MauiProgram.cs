b3b7d7d90b7d785e117cf6a4440b01d577ce37fe
Noah Teubner <nteubner@schueler.hakzell.at>
2024-04-11 10:59:21 +0200

    added alterservice in MauiProgram.cs

--- Studiensammlung.StudiensammlungApp/MauiProgram.cs
+++ Studiensammlung.StudiensammlungApp/MauiProgram.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 using Microsoft.Maui.Controls.Compatibility.Hosting;
 using Studiensammlung.Lib;
+using Studiensammlung.StudiensammlungApp.Services;
 using Studiensammlung.StudiensammlungApp.ViewModels;
 using Studiensammlung.StudiensammlungCore.Services;
 
@@ -13,6 +15,7 @@ namespace Studiensammlung.StudiensammlungApp
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
@@ -32,7 +35,7 @@ namespace Studiensammlung.StudiensammlungApp
 
             builder.Services.AddSingleton<IRepository>(new SqliteRepository(fullpath));
 
-
+            builder.Services.AddSingleton<IAlertService, AlertService>();
 
 #if DEBUG
             builder.Logging.AddDebug();
