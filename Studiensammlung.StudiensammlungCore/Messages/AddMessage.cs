bda2177160eef9df6620287e462142d7e436fa60
Noah Teubner <nteubner@schueler.hakzell.at>
2024-04-11 08:34:21 +0200

    created the path for the data.sqlite

---
+++ Studiensammlung.StudiensammlungCore/Messages/AddMessage.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+using Studiensammlung.Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studiensammlung.StudiensammlungCore.Messages;
+
+public class AddMessage : ValueChangedMessage<Entry>
+{
+    public AddMessage(Entry value) : base(value)
+    {
+    }
+}
--- Studiensammlung.StudiensammlungCore/ViewModels/MainViewModel.cs
+++ Studiensammlung.StudiensammlungCore/ViewModels/MainViewModel.cs
@@ -1,11 +1,40 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Studiensammlung.Lib;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Studiensammlung.StudiensammlungApp.ViewModels;
 
-public partial class MainViewModel
+public partial class MainViewModel(IRepository repository) : ObservableObject
 {
+    public string Header => "Studiensammlung";
+    IRepository _repository = repository;
+    private bool _isLoaded = false;
+
+    [ObservableProperty]
+    ObservableCollection<Lib.Entry> _entries = [];
+
+    [ObservableProperty]
+    Lib.Entry? _selectedEntry = null;
+
+    #region Properties
+
+
+    [ObservableProperty]
+    string _user = string.Empty;
+    string _password = string.Empty;
+    string _studyCourse = string.Empty;
+    int _studyLength = 0;
+    string _title = string.Empty;
+
+    #endregion
+
+
+
+
+
 }
